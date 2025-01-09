c5aeeca75717a8e9a44fe57a2fc96ca3bb3a7ba8
Matteo Tagliatti <matteotagliatti@gmail.com>
2025-01-09 14:46:09 +0100

    more buttons

--- app/buttons/page.tsx
+++ app/buttons/page.tsx
@@ -2,7 +2,12 @@ import DemoComponent from "@/components/demo/demo-component";
 import { PageContainer } from "@/components/demo/page-container";
 
 const DIR = "buttons";
-const FILES = ["action-button", "group"];
+const FILES = [
+  "action-button",
+  "group",
+  "interactive-hover-button",
+  "animated-submit-button",
+];
 
 export default async function ButtonsPage() {
   return (
