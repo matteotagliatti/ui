32e0b4a05cdada1115cba8817e4e77754a36bffd
Matteo Tagliatti <matteotagliatti@gmail.com>
2025-01-13 17:46:14 +0100

    fix font

--- app/layout.tsx
+++ app/layout.tsx
@@ -44,7 +44,7 @@ export default async function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased font-sans`}
       >
         <Providers sidebarDefaultOpen={sidebarDefaultOpen}>
           <AppSidebar />
