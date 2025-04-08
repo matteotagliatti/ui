295f3d73825dfb8479eea43f045e83a5e702191b
Matteo Tagliatti <matteotagliatti@gmail.com>
2025-04-08 17:34:04 +0200

    restart and updates

--- next.config.ts
+++ next.config.ts
@@ -1,18 +1,7 @@
 import type { NextConfig } from "next";
-import { version } from "./package.json";
 
 const nextConfig: NextConfig = {
-  env: {
-    package_version: version,
-  },
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "avatars.githubusercontent.com",
-      },
-    ],
-  },
+  /* config options here */
 };
 
 export default nextConfig;
