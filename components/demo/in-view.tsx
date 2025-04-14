ba4ab3db1e84a34f3a4482c604fa0fa0623c4a95
Matteo Tagliatti <matteotagliatti@gmail.com>
2025-04-14 10:43:39 +0200

    in view component

---
+++ components/demo/in-view.tsx
@@ -0,0 +1,26 @@
+import { InView } from "@/registry/default/components/in-view";
+
+export default function InViewDemo() {
+  return (
+    <div className="h-[350px] w-full overflow-auto">
+      <div className="py-12 text-center text-sm">Scroll down</div>
+      <div className="flex h-[500px] items-end justify-center px-4 pb-24">
+        <InView
+          variants={{
+            hidden: { opacity: 0, y: 100, filter: "blur(4px)" },
+            visible: { opacity: 1, y: 0, filter: "blur(0px)" },
+          }}
+          viewOptions={{ margin: "0px 0px -100px 0px" }}
+          transition={{ duration: 0.3, ease: "easeInOut" }}
+        >
+          <div className="max-w-96">
+            <p className="text-white">
+              <span className="font-medium">Title</span>: Lorem ipsum dolor sit
+              amet consectetur adipisicing elit. Quisquam, quos.
+            </p>
+          </div>
+        </InView>
+      </div>
+    </div>
+  );
+}
