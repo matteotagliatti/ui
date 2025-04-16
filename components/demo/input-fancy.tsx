7106388a7e60fc1653aef73c17eed5f1acae898d
Matteo Tagliatti <matteotagliatti@gmail.com>
2025-04-16 11:32:46 +0200

    more input fancy components

--- components/demo/input-fancy.tsx
+++ components/demo/input-fancy.tsx
@@ -1,7 +1,15 @@
 import { InputFancy } from "@/registry/default/components/input-fancy";
-import { User } from "lucide-react";
-import { InputContainer } from "../input-container";
-import { Label } from "../ui/label";
+import { Send, User } from "lucide-react";
+import { InputContainer } from "@/components/input-container";
+import { Label } from "@/components/ui/label";
+import { Button } from "@/components/ui/button";
+import {
+  Select,
+  SelectTrigger,
+  SelectValue,
+  SelectContent,
+  SelectItem,
+} from "@/components/ui/select";
 
 export default function InputFancyDemo() {
   return (
@@ -9,6 +17,7 @@ export default function InputFancyDemo() {
       <InputContainer>
         <Label htmlFor="input-fancy">Input Fancy (with icon)</Label>
         <InputFancy
+          inline
           left={<User className="h-4 w-4" />}
           placeholder="Input Fancy"
           type="text"
@@ -24,10 +33,40 @@ export default function InputFancyDemo() {
         />
       </InputContainer>
       <InputContainer>
-        <Label htmlFor="input-fancy">Input Fancy (inline)</Label>
+        <Label htmlFor="input-fancy">Input Fancy (with button)</Label>
         <InputFancy
           inline
-          left={<User className="h-4 w-4" />}
+          rightClassName="border-none p-0"
+          right={
+            <Button
+              className="rounded-l-none !bg-transparent"
+              variant="outline"
+              size="icon"
+            >
+              <Send className="h-4 w-4" />
+            </Button>
+          }
+          placeholder="Input Fancy"
+          type="text"
+        />
+      </InputContainer>
+      <InputContainer>
+        <Label htmlFor="input-fancy">Input Fancy (with select)</Label>
+        <InputFancy
+          inline
+          leftClassName="border-none p-0"
+          left={
+            <Select>
+              <SelectTrigger className="hover:text-primary w-[100px] rounded-r-none !bg-transparent">
+                <SelectValue placeholder="Select" />
+              </SelectTrigger>
+              <SelectContent>
+                <SelectItem value="apple">Apple</SelectItem>
+                <SelectItem value="banana">Banana</SelectItem>
+                <SelectItem value="cherry">Cherry</SelectItem>
+              </SelectContent>
+            </Select>
+          }
           placeholder="Input Fancy"
           type="text"
         />
