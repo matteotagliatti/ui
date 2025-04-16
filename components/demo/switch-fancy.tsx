b7cc5e754995926c76216b518ca3e74ec1a51825
Matteo Tagliatti <matteotagliatti@gmail.com>
2025-04-16 14:25:03 +0200

    better options

--- components/demo/switch-fancy.tsx
+++ components/demo/switch-fancy.tsx
@@ -1,9 +1,9 @@
 import { SwitchFancy } from "@/registry/default/components/switch-fancy";
 
 const options = [
-  { label: "Option 1", value: "option1" },
-  { label: "Option 2", value: "option2" },
-  { label: "Option 3", value: "option3" },
+  { label: "Opt. 1", value: "option1" },
+  { label: "Opt. 2", value: "option2" },
+  { label: "Opt. 3", value: "option3" },
 ];
 
 export default function SwitchFancyDemo() {
